fbe5252a8b31f5947a54482dae9168e1d351ef23
MD IZHAR <149708483+Izhar66@users.noreply.github.com>
2025-12-03 17:59:31 +0000

    recommit

--- BasicOfJavascript/02_DataTypes.js
+++ BasicOfJavascript/02_DataTypes.js
@@ -1,7 +1,7 @@
-const name="Md Izhar";
-let ag=22;
+// const name="Md Izhar";
+// let ag=22;
 
-console.log(name);
+// console.log(name);
 /*
 Data Types
 number=123
@@ -14,5 +14,25 @@ object
 */
 
 
-console.log(typeof null);
-console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+let functions=function(){
+    console.log("Hello world")
+}
+
+// functions()
+// console.log(typeof functions);
+
+
+userOne={
+    name: "izhar",
+    email: "izhar.md@amshuhu.com",
+    designation: "junior software developer",
+}
+
+userTwo=userOne
+userTwo.email="izhar@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email);
