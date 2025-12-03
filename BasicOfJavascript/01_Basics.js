a137df8bedf5ed6cb9e604575e252b8cd35db1e6
MD IZHAR <149708483+Izhar66@users.noreply.github.com>
2025-12-03 14:47:48 +0000

    Second commit

---
+++ BasicOfJavascript/01_Basics.js
@@ -0,0 +1,6 @@
+const account_id=1234343;
+let account_holder_name="Izhar";
+var Designation="junior software develpment";
+City_name="Motihari";
+
+console.table([account_id,account_holder_name,Designation,City_name]);
---
+++ BasicOfJavascript/02_DataTypes.js
@@ -0,0 +1,18 @@
+const name="Md Izhar";
+let ag=22;
+
+console.log(name);
+/*
+Data Types
+number=123
+Bigint
+String=""
+Boolea = true/false
+undefined
+null
+object
+*/
+
+
+console.log(typeof null);
+console.log(typeof undefined);
